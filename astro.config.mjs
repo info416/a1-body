c3f21ae28078b905f2b1ebcc2f850532d285cc66
ibuki inamoto <info@ai-verve.net>
2025-09-14 13:24:08 +0900

    焦土作戦実行

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless"; // Vercelの神官を召喚
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: 'server', // ← 生命の創造を許可する、神聖な勅令
   adapter: vercel()   // ← Vercelの神官に、正しく生命を扱うよう指示
 });
